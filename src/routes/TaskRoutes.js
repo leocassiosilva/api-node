36067836cb4fb245173db28b987686e62f4e4c4a
leocassiosilva <leocassiosilva1234@gmail.com>
2022-05-30 12:16:14 -0300

    update task

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -6,6 +6,7 @@ const TaskContoller = require('../controller/TaskContoller');
 const TaskValidation = require('../middlewares/TaskValidation');
 
 router.post('/', TaskValidation, TaskContoller.create);
+router.put('/:id', TaskContoller.update);
 
 
 module.exports = router;
