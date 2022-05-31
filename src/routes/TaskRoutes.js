fd7120689124e2154cb0ed100fb8063a88f02067
leocassiosilva <leocassiosilva1234@gmail.com>
2022-05-31 09:51:32 -0300

    list taks especifica

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -9,6 +9,7 @@ const MacaddressValidation = require('../middlewares/MacaddressValidation');
 router.post('/', TaskValidation, TaskContoller.create);
 router.put('/:id', TaskValidation, TaskContoller.update);
 router.get('/filter/all', MacaddressValidation, TaskContoller.all);
+router.get('/:id', TaskContoller.show);
 
 
 module.exports = router;
